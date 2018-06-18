539ebfb864c0c1a4e60c4c20151a65d246dd6efd
Marni <marniduffy@gmail.com>
2018-06-18 14:18:36 -0400

    hid firebase config data

--- src/App.js
+++ src/App.js
@@ -5,16 +5,8 @@ import PollsPage from './PollsPage';
 import NotFound from './NotFound';
 import PollPage from './PollPage';
 import NewPoll from './NewPoll';
+import config from './firebaseConfig';
 const firebase = require('firebase/app');
-
-const config = {
-  apiKey: 'AIzaSyCKg28QO97IFsRTWt1z2UFA5LEdXslIiMw',
-  authDomain: 'poll-app-web.firebaseapp.com',
-  databaseURL: 'https://poll-app-web.firebaseio.com',
-  projectId: 'poll-app-web',
-  storageBucket: 'poll-app-web.appspot.com',
-  messagingSenderId: '1036908880226',
-};
 
 firebase.initializeApp(config);
 
