96686d46c83aa0722b3524227cf75279c69b3c2d
Marni <marniduffy@gmail.com>
2018-06-18 14:26:35 -0400

    added firebase credentials

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'AIzaSyCKg28QO97IFsRTWt1z2UFA5LEdXslIiMw',
+  authDomain: 'poll-app-web.firebaseapp.com',
+  databaseURL: 'https://poll-app-web.firebaseio.com',
+  projectId: 'poll-app-web',
+  storageBucket: 'poll-app-web.appspot.com',
+  messagingSenderId: '1036908880226',
+};
+
+module.exports = config; 
